d1121f8009f700649b3bb88d4c35f1bbcb9226ee
Sojung Pyeon <112847034+jesssojung@users.noreply.github.com>
2022-09-07 14:03:03 +0900

    Update product_user_query.sql

--- product_user_query.sql
+++ product_user_query.sql
@@ -11,17 +11,17 @@ FROM
 		FROM product
 		GROUP BY brand_name, category3_name) prd
 	LEFT JOIN
-					(
-				SELECT brand_name brd_name
-                     , SUM(F) F
-					 , SUM(M) M
-					 , ROUND(AVG(age), 1) age
-				FROM product p 
-					 INNER JOIN event e
-					 ON p.item_no = e.item_no
-					 INNER JOIN user u
-					 ON e.user_no = u.user_no
-				WHERE event_name = 'purchase_success'
-				GROUP BY brand_name
-				) user
+		(
+		SELECT brand_name brd_name
+			 , SUM(F) F
+			 , SUM(M) M
+			 , ROUND(AVG(age), 1) age
+		FROM product p 
+			 INNER JOIN event e
+			 ON p.item_no = e.item_no
+			 INNER JOIN user u
+			 ON e.user_no = u.user_no
+		WHERE event_name = 'purchase_success'
+		GROUP BY brand_name
+		) user
 		ON prd.brd_name = user.brd_name
