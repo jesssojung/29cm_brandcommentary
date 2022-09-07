6c99373cd3f6e56fab7a4330568a9a507fda500d
Sojung Pyeon <112847034+jesssojung@users.noreply.github.com>
2022-09-07 16:23:28 +0900

    Update product_user_query.sql

--- product_user_query.sql
+++ product_user_query.sql
@@ -1,21 +1,21 @@
-SELECT prd.brd_name
-	 , ct3_name
-     , item_cnt
-     , F/(F+M) f_pct
-     , M/(F+M) m_pct
+SELECT prd.brd_name -- 브랜드
+	 , ct3_name -- 소분류
+     , item_cnt -- 브랜드 소분류별 아이템 갯수
+     , F/(F+M) f_pct -- 브랜드 아이템 구매 유저 여성 비율
+     , M/(F+M) m_pct -- 브랜드 아이템 구매 유저 남성 비율
      , age
 FROM
 		(SELECT brand_name brd_name
-			 , category3_name ct3_name
-			 , COUNT(DISTINCT item_no) item_cnt
+		      , category3_name ct3_name
+		      , COUNT(DISTINCT item_no) item_cnt
 		FROM product
 		GROUP BY brand_name, category3_name) prd
 	LEFT JOIN
-		(
+			(
 		SELECT brand_name brd_name
-			 , SUM(F) F
-			 , SUM(M) M
-			 , ROUND(AVG(age), 1) age
+     		     , SUM(F) F
+		     , SUM(M) M
+		     , ROUND(AVG(age), 1) age
 		FROM product p 
 			 INNER JOIN event e
 			 ON p.item_no = e.item_no
